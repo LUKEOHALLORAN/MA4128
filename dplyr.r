1599aec41d5414e0c296283cf895aa65d80d3686
LUKEOHALLORAN <35770979+LUKEOHALLORAN@users.noreply.github.com>
2018-01-24 17:40:45 +0000

    Update dplyr.r

--- dplyr.r
+++ dplyr.r
@@ -2,3 +2,14 @@ iris %>% group_by(Species) %>% summarize(mean(Sepal.Length)) iris %>% group_by(S
 iris %>% group_by(Species) %>% summarize(meanSL = mean(Sepal.Length),        varSL =var(Sepal.Length),                                            sdSL =sd(Sepal.Length)) 
 mtcarsmtcars %>% group_by(cyl) %>% summarize(mean(mpg))mtcars %>% group_by(vs,am) %>% summarize(medianMPG = median(mpg))help(mtcars)
 mtcars %>% select(2:4, everything() )mtcars %>% mutate(sqrtMPG = sqrt(mpg))mtcars %>% mutate(sqrtQSEC = sqrt(qsec)) %>% head(5)mtcars %>% mutate(sqrtMPG = sqrt(mpg), logMPG = log(mpg)) %>%                  select(mpg, sqrtMPG, logMPG, everything() )
+
+install.packages("readr") install.packages("modelr") install.packages("randomForest") install.packages("ggplot2") 
+ library(readr) library(modelr) library(randomForest) library(ggplot2) 
+data(diamonds)diamondsdiamonds %>% dim()diamonds %>% names()
+diamtrain <- diamonds %>% sample_frac(0.30)diamtest <- diamonds %>% sample_frac(0.10)
+diamtrain %>% dim()diamtest %>% dim()
+pricemodel <- randomForest (price ~ . , data = diamtrain)pricemodel
+pricemodelLM <- lm (price ~ . , data = diamtrain)pricemodelLM
+rmse(pricemodelLM, diamtrain)rmse(pricemodelLM, diamtest)
+mae(pricemodelLM, diamtrain)mae(pricemodelLM, diamtest)
+rsquare(pricemodelLM, diamtrain)rsquare(pricemodelLM, diamtest)
